e5083062541a878b13bb38a0019d793599fdc7b6
nyanthu-okabe <nyanthuokabe@gmail.com>
2025-10-25 09:26:00 +0900

    thied

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
 from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder="templates")
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    user = {"activate": False, "name": "John Doe"}
+    return render_template("index.html", user=user)
 
-@app.route('/<page>/')
+
+@app.route("/<page>/")
 def show(page):
+    user = {"activate": True, "name": "John Doe"}
     try:
-        return render_template(f'{page}/index.html')
+        return render_template(f"{page}/index.html", user=user)
     except:
-        return render_template('404.html'), 404
+        return render_template("404.html"), 404
 
-@app.route('/static/<path:path>')
+
+@app.route("/static/<path:path>")
 def send_static(path):
-    return send_from_directory('static', path)
+    return send_from_directory("static", path)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
