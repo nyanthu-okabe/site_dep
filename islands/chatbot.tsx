c771418ae43e5d7bc62ac6cc2d348bf5d74b674d
nyanthu-okabe <nyanthuokabe@gmail.com>
2025-11-02 16:26:12 +0900

    Create chatbot.tsx

---
+++ islands/chatbot.tsx
@@ -0,0 +1,9 @@
+export default function Chatbot() {
+  return (
+    <section id="chatbot">
+      <iframe src="https://nyanthubot.nyanthu.com/">
+      </iframe>
+      <a href="https://nyanthubot.nyanthu.com">nyanthubot.nyanthu.com</a>
+    </section>
+  );
+}
